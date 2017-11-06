b421fac88fe1439351051039910b192fcd5d0b9c
tugudush <jerome2kph@gmail.com>
2017-11-06 19:55:13 +0800

    create form

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -9,8 +9,16 @@
 
 @section('content')
   <div class="col-sm-8 blog-main">
-    
-  </div><!--/.blog-main-->
+    <form action="" method="post">
+      <div class="form-group">
+        <input type="text" id="input-post-title" name="input_post_title" class="form-control" placeholder="Title"/>
+      </div><!--/.form-group-->
+      <div class="form-group">
+        <textarea id="input-post-body" name="input_post_body" class="form-control" rows="8" placeholder="Content"></textarea>
+      </div><!--/.form-group-->
+      <button type="submit" class="btn btn-primary">Submit</button>      
+    </form>
+  </div>
 @endsection
 
 @section('page-specific-footer-scripts')
