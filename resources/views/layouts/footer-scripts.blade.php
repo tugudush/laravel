3cc6a34acf21a97ec083025195f0c6676a4b70a0
tugudush <jerome2kph@gmail.com>
2017-11-06 10:52:02 +0800

    remove whitespace on footer-scripts.blade.php

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -1,4 +1,4 @@
 <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
 	<script src="{{ URL::asset('js/jquery-3.2.1.min.js') }}"></script>
 	<script src="{{ URL::asset('js/functions.js') }}"></script>
-	<script src="{{ URL::asset('js/frontend.js') }}"></script>	
+	<script src="{{ URL::asset('js/frontend.js') }}"></script>
